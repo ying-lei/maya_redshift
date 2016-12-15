02bf11c4895d2dd8c6cb7d5fe050ba0622d826b1
Yinglei Yang <contact.yinglei@gmail.com>
2016-12-14 21:48:32 -0800

    Adding startup renderGlobal status check

--- load_aovs/load_aovs.py
+++ load_aovs/load_aovs.py
@@ -1,5 +1,5 @@
 # ==================== Redshift Load AOV's ====================
-# Create a custom UI to load AOV into Maya RenderView
+# Create a custom UI to load Redshift AOV into Maya RenderView
 # LIMITATION: Doesn't work on Token <Camera>
     
 
@@ -7,35 +7,66 @@ import maya.cmds as cmds
 import maya.mel as mel 
 import os
 
-# Main UI 
-def aovUI():
-    getActiveAOVS() 
-    
-    if cmds.window('aovWindow', ex=1):
-        cmds.deleteUI('aovWindow')
-    
-    cmds.window('aovWindow', w=150,h=100,s=0)
-    cmds.formLayout('wndLayout')
- 
-    descAov = cmds.text( label="Redshift Load AOV's",w=150,h=22,fn='boldLabelFont',bgc=(0.15,0.15,0.15))
-    aovsList = cmds.optionMenu( 'aovsListMenu',w=140,label='AOV',cc=loadAOV )
-    renderButton = cmds.button(w=150, label='RENDER', command=renderButtonPush)
-    refreshButton = cmds.button(w=150, label='REFRESH UI', command=refreshButtonPush)
- 
-    cmds.menuItem(label='Beauty')
- 
-    for item in aovNames:
-        cmds.menuItem(label=item )
- 
-    #Attach controls
-    cmds.formLayout('wndLayout',e=1,af=[ (descAov,'top',0), (descAov,'left',0) ])
-    cmds.formLayout('wndLayout',e=1,af=[ (aovsList,'top',25), (aovsList,'left',5) ])
-    cmds.formLayout('wndLayout',e=1,af=[ (renderButton,'top',50) ])
-    cmds.formLayout('wndLayout',e=1,af=[ (refreshButton,'top',75) ])
 
-    cmds.showWindow('aovWindow')
- 
-# Formatting AOV file path
+########## Startup Checking ##########
+def checkRenderEnvSettings():
+    # Check current renderer
+    if cmds.getAttr("defaultRenderGlobals.currentRenderer") != 'redshift':
+        cmds.warning("Redshift is not current renderer")
+        return
+    
+    # Check unsupported tokens:
+    imgPath = cmds.getAttr("defaultRenderGlobals.imageFilePrefix")
+    imgPath = str(imgPath)
+    def findInvalidString():
+        for line in imgPath.split("\n"):
+            if "<Camera>" in line:
+                return True
+    if findInvalidString()== True:
+        cmds.warning("Token '<Camera>' is not supported")
+        return
+
+    # Check image format:
+    if cmds.getAttr("redshiftOptions.imageFormat") != 1:
+        cmds.warning("Only .exr format is supported")
+        return
+    
+    # Check AOV enable
+    if cmds.getAttr("redshiftOptions.aovGlobalEnableMode") != 1:
+        cmds.warning("Please enable AOV render (not Batch Only)")
+        return
+    
+    cmds.loadPlugin('OpenEXRLoader.mll')
+    aovUI()
+
+
+########## Update AOV list ##########
+def getActiveAOVS():
+    createdAOVS = cmds.ls(et='RedshiftAOV')
+
+    global activeAOVS
+    global aovNames
+        
+    activeAOVS = []
+    aovNames = []
+    
+    del activeAOVS[:]
+    del aovNames[:]
+
+    for item in createdAOVS:
+        if cmds.getAttr("{0}.enabled".format(item))!= 0:
+            activeAOVS.append( item )
+            aovNames.append( item.split('_', 1)[1])
+   
+    if activeAOVS == []:
+        cmds.warning("No aov's setup")
+        return
+
+    if cmds.getAttr("redshiftOptions.aovGlobalEnableMode") != 1:
+        cmds.warning("Please enable AOV render (not Batch Only)")
+        return
+
+########## Formatting AOV file path ##########
 def loadAOV(value):
     getActiveAOVS()
 
@@ -72,11 +103,42 @@ def loadAOV(value):
     print "Loaded: %s" % fullPath
     cmds.renderWindowEditor(rview, e=True, li=fullPath)
 
-# UI: RENDER button
+
+########## Main UI ##########
+def aovUI():
+    getActiveAOVS() 
+    cmds.warning("AOV will be available after render (not IPR)")
+    
+    if cmds.window('aovWindow', ex=1):
+        cmds.deleteUI('aovWindow')
+    
+    cmds.window('aovWindow', w=150,h=100,s=0)
+    cmds.formLayout('wndLayout')
+ 
+    descAov = cmds.text( label="Redshift Load AOV's",w=150,h=22,fn='boldLabelFont',bgc=(0.15,0.15,0.15))
+    aovsList = cmds.optionMenu( 'aovsListMenu',w=140,label='AOV',cc=loadAOV )
+    renderButton = cmds.button(w=150, label='RENDER', command=renderButtonPush)
+    refreshButton = cmds.button(w=150, label='REFRESH UI', command=refreshButtonPush)
+ 
+    cmds.menuItem(label='Beauty')
+ 
+    for item in aovNames:
+        cmds.menuItem(label=item )
+ 
+    #Attach controls
+    cmds.formLayout('wndLayout',e=1,af=[ (descAov,'top',0), (descAov,'left',0) ])
+    cmds.formLayout('wndLayout',e=1,af=[ (aovsList,'top',25), (aovsList,'left',5) ])
+    cmds.formLayout('wndLayout',e=1,af=[ (renderButton,'top',50) ])
+    cmds.formLayout('wndLayout',e=1,af=[ (refreshButton,'top',75) ])
+
+    cmds.showWindow('aovWindow')
+ 
+
+########## UI: REFRESH button##########
 def refreshButtonPush(*args):
   aovUI()
 
-# UI: REFRESH button
+########## UI: RENDER button ##########
 def renderButtonPush(*args):
 
     getActiveAOVS()
@@ -86,6 +148,11 @@ def renderButtonPush(*args):
     if activeAOVS == []:
         cmds.warning("No aov's setup")
         return
+    
+    if cmds.getAttr("redshiftOptions.aovGlobalEnableMode") != 1:
+        cmds.warning("Please enable AOV render (not Batch Only)")
+        return
+    
     aovNameInitSetting = cmds.getAttr('{0}.filePrefix'.format(activeAOVS[0]))
     
     # Ovveride
@@ -107,35 +174,5 @@ def renderButtonPush(*args):
 
     aovUI()
 
-
-# On startup
-def checkRenderEnvSettings():
-    if cmds.getAttr("defaultRenderGlobals.currentRenderer") != 'redshift':
-        cmds.warning("Redshift is not current renderer")
-        return
-    cmds.loadPlugin('OpenEXRLoader.mll')
-    aovUI()
-
-# Update AOV list    
-def getActiveAOVS():
-    createdAOVS = cmds.ls(et='RedshiftAOV')
-    
-    activeAOVS = []
-    aovNames = []
-
-    global activeAOVS
-    global aovNames
-    
-    del activeAOVS[:]
-    del aovNames[:]
-
-    for item in createdAOVS:
-        if cmds.getAttr("{0}.enabled".format(item))!= 0:
-            activeAOVS.append( item )
-            aovNames.append( item.split('_', 1)[1])
-   
-    if activeAOVS == []:
-        cmds.warning("No aov's setup")
-        return
 
 checkRenderEnvSettings()
